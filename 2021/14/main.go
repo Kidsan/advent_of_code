91e9ae5f791c655aa56e556c4bdacc45242152cc
Kieran O'Sullivan <kieran.osullivan@rewe-digital.com>
2021-12-14 13:30:11 +0100

    2021: day 14 part 1 and 2

--- 2021/14/main.go
+++ 2021/14/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 )
 
 type rule struct {
@@ -12,114 +13,61 @@ type rule struct {
 	inserts string
 }
 
-type Letter struct {
-	next  *Letter
-	value string
-}
-
-type InputString struct {
-	start  *Letter
-	length int
-}
-
 type insertion struct {
-	pointer *Letter
-	new     *Letter
+	value    string
+	replaces string
+	number   int
 }
 
-func (i *InputString) add(newLetter string) {
-	new := &Letter{value: newLetter, next: i.start}
-	i.start = new
-	i.length++
-}
-
-func (i *InputString) toString() string {
-	result := ""
-
-	iter := i.start
-	for iter != nil {
-		result += iter.value
-		iter = iter.next
-	}
-
-	return result
-}
-
-func insertionPhase(input *InputString, rules []rule) {
+func insertionPhase(input map[string]int, rules []rule) {
 	insertions := make([]insertion, 0)
 	for _, r := range rules {
-		current := input.start
-		for current != nil {
-			oldNext := current.next
-			if oldNext != nil {
-				if current.value == string(r.matches[0]) && oldNext.value == string(r.matches[1]) {
-					newInsertion := &Letter{value: r.inserts, next: oldNext}
-					insertions = append(insertions, insertion{pointer: current, new: newInsertion})
-					input.length++
-
-				}
-			}
-			current = oldNext
+		count, ok := input[r.matches]
+		if !ok {
+			continue
 		}
+		insertions = append(insertions, insertion{
+			value:    fmt.Sprintf("%s%s", string(r.matches[0]), r.inserts),
+			replaces: r.matches,
+			number:   count,
+		})
+		insertions = append(insertions, insertion{
+			value:    fmt.Sprintf("%s%s", r.inserts, string(r.matches[1])),
+			replaces: r.matches,
+			number:   count,
+		})
+		delete(input, r.matches)
 	}
 
-	for _, i := range insertions {
-		l := i.pointer
-		l.next = i.new
+	for _, v := range insertions {
+		input[v.value] += v.number
 	}
+
 }
 
-func insertions(input *InputString, rules []rule, iterations int) string {
+func insertions(input map[string]int, rules []rule, iterations int) map[string]int {
 	for i := 0; i < iterations; i++ {
 		insertionPhase(input, rules)
 	}
-	return input.toString()
+	return input
 }
 
-func part1(input string, rules []rule) int {
-	iterations := 10
-	inputList := InputString{}
-	for i := len(input) - 1; i >= 0; i-- {
-		inputList.add(string(input[i]))
-	}
+func applyRules(input string, rules []rule, iterations int) int {
+	chars := make(map[string]int)
 
-	insertions(&inputList, rules, iterations)
+	for i := 0; i+1 < len(input); i++ {
+		newEntry := string(input[i]) + string(input[i+1])
+		chars[newEntry]++
+	}
+	chars[string(input[len(input)-1])] = 1
+
+	insertions(chars, rules, iterations)
 
 	values := make(map[string]int)
-	curr := inputList.start
-	for curr != nil {
-		values[curr.value]++
-		curr = curr.next
-	}
 
-	most := 0
-	least := 0
-	for _, value := range values {
-		if least == 0 || value < least {
-			least = value
-		}
-		if value > most {
-			most = value
-		}
-	}
-
-	return most - least
-}
-
-func part2(input string, rules []rule) int {
-	iterations := 40
-	inputList := InputString{}
-	for i := len(input) - 1; i >= 0; i-- {
-		inputList.add(string(input[i]))
-	}
-
-	insertions(&inputList, rules, iterations)
-
-	values := make(map[string]int)
-	curr := inputList.start
-	for curr != nil {
-		values[curr.value]++
-		curr = curr.next
+	for key, count := range chars {
+		letters := strings.Split(key, "")
+		values[letters[0]] += count
 	}
 
 	most := 0
@@ -137,6 +85,7 @@ func part2(input string, rules []rule) int {
 }
 
 func main() {
+	start := time.Now()
 	input, err := os.Open("./input.txt")
 	if err != nil {
 		panic(err)
@@ -159,6 +108,8 @@ func main() {
 		})
 	}
 
-	fmt.Println(part1(inputText, rules))
-	fmt.Println(part2(inputText, rules))
+	fmt.Println(applyRules(inputText, rules, 10))
+	fmt.Printf("part one took %s\n", time.Since(start))
+	fmt.Println(applyRules(inputText, rules, 40))
+	fmt.Printf("part two took %s\n", time.Since(start))
 }
--- 2021/14/main_test.go
+++ 2021/14/main_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestInsertions(t *testing.T) {
 	tt := []struct {
 		input         string
-		expectedValue string
+		expectedValue map[string]int
 		rules         []rule
 	}{
 		{
@@ -78,27 +79,32 @@ func TestInsertions(t *testing.T) {
 					inserts: "C",
 				},
 			},
-			expectedValue: "NCNBCHB",
+			expectedValue: map[string]int{"B": 1, "BC": 1, "CH": 1, "CN": 1, "HB": 1, "NB": 1},
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run("TestInsertions", func(t *testing.T) {
-			inputList := InputString{}
-			for i := len(tc.input) - 1; i >= 0; i-- {
-				inputList.add(string(tc.input[i]))
-			}
-			actual := insertions(&inputList, tc.rules, 1)
+			chars := make(map[string]int)
 
-			if actual != tc.expectedValue {
-				t.Errorf("Got %s, expected %s", actual, tc.expectedValue)
+			for i := 0; i+1 < len(tc.input); i++ {
+				newEntry := string(tc.input[i]) + string(tc.input[i+1])
+				chars[newEntry]++
+			}
+			chars[string(tc.input[len(tc.input)-1])] = 1
+			actual := insertions(chars, tc.rules, 1)
+			fmt.Println(actual)
+			for evKey, ev := range tc.expectedValue {
+				if actual[evKey] != ev {
+					t.Errorf("Got %d, expected %d", actual[evKey], ev)
+				}
 			}
 
 		})
 	}
 }
 
-func TestPart1(t *testing.T) {
+func TestApplyRules10Times(t *testing.T) {
 	tt := []struct {
 		input         string
 		rules         []rule
@@ -177,8 +183,8 @@ func TestPart1(t *testing.T) {
 	}
 
 	for _, tc := range tt {
-		t.Run("TestPart1", func(t *testing.T) {
-			actual := part1(tc.input, tc.rules)
+		t.Run("TestApplyRules 10 Times", func(t *testing.T) {
+			actual := applyRules(tc.input, tc.rules, 10)
 			if actual != tc.expectedValue {
 				t.Errorf("Got %v, expected %v", actual, tc.expectedValue)
 			}
@@ -187,250 +193,90 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-// func TestFoldX(t *testing.T) {
-// 	tt := []struct {
-// 		input         [][]int
-// 		expectedValue [][]int
-// 		foldLine      int
-// 	}{
-// 		{
-// 			input: [][]int{
-// 				{1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0},
-// 				{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-// 				{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-// 				{0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 			},
-// 			foldLine: 5,
-// 			expectedValue: [][]int{
-// 				{1, 1, 1, 1, 1},
-// 				{1, 0, 0, 0, 1},
-// 				{1, 0, 0, 0, 1},
-// 				{1, 0, 0, 0, 1},
-// 				{1, 1, 1, 1, 1},
-// 				{0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0},
-// 			},
-// 		},
-// 	}
+func TestApplyRules(t *testing.T) {
+	tt := []struct {
+		input         string
+		rules         []rule
+		expectedValue int
+	}{
+		{
+			input: "NNCB",
+			rules: []rule{
+				{
+					matches: "CH",
+					inserts: "B",
+				},
+				{
+					matches: "HH",
+					inserts: "N",
+				},
+				{
+					matches: "CB",
+					inserts: "H",
+				},
+				{
+					matches: "NH",
+					inserts: "C",
+				},
+				{
+					matches: "HB",
+					inserts: "C",
+				},
+				{
+					matches: "HC",
+					inserts: "B",
+				},
+				{
+					matches: "HN",
+					inserts: "C",
+				},
+				{
+					matches: "NN",
+					inserts: "C",
+				},
+				{
+					matches: "BH",
+					inserts: "H",
+				},
+				{
+					matches: "NC",
+					inserts: "B",
+				},
+				{
+					matches: "NB",
+					inserts: "B",
+				},
+				{
+					matches: "BN",
+					inserts: "B",
+				},
+				{
+					matches: "BB",
+					inserts: "N",
+				},
+				{
+					matches: "BC",
+					inserts: "B",
+				},
+				{
+					matches: "CC",
+					inserts: "N",
+				},
+				{
+					matches: "CN",
+					inserts: "C",
+				},
+			},
+			expectedValue: 2188189693529,
+		},
+	}
 
-// 	for _, tc := range tt {
-// 		t.Run("TestFoldX", func(t *testing.T) {
-// 			actual := foldX(tc.input, tc.foldLine)
-
-// 			for y, line := range tc.expectedValue {
-// 				for x, value := range line {
-// 					if value != actual[y][x] {
-// 						t.Errorf("Got %d, expected %d", value, tc.expectedValue[y][x])
-// 					}
-// 				}
-// 			}
-
-// 		})
-// 	}
-// }
-
-// func TestPart1(t *testing.T) {
-// 	tt := []struct {
-// 		input         [][]int
-// 		foldLine      fold
-// 		expectedValue int
-// 	}{
-// 		{
-// 			input: [][]int{
-// 				{0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0},
-// 				{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0},
-// 				{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-// 				{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-// 				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-// 				{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-// 			},
-// 			foldLine:      fold{dimension: "y", line: 7},
-// 			expectedValue: 17,
-// 		},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run("TestPart1", func(t *testing.T) {
-// 			actual := part1(tc.input, tc.foldLine)
-
-// 			if actual != tc.expectedValue {
-// 				t.Errorf("Got %d, expected %d", actual, tc.expectedValue)
-// 			}
-
-// 		})
-// 	}
-// }
-
-// func TestPart2(t *testing.T) {
-// 	tt := []struct {
-// 		input         []string
-// 		expectedValue int
-// 	}{
-// 		{
-// 			input: []string{
-// 				"[({(<(())[]>[[{[]{<()<>>",
-// 				"[(()[<>])]({[<{<<[]>>(",
-// 				"{([(<{}[<>[]}>{[]{[(<()>",
-// 				"(((({<>}<{<{<>}{[]{[]{}",
-// 				"[[<[([]))<([[{}[[()]]]",
-// 				"[{[{({}]{}}([{[{{{}}([]",
-// 				"{<[[]]>}<{[{[{[]{()[[[]",
-// 				"[<(<(<(<{}))><([]([]()",
-// 				"<{([([[(<>()){}]>(<<{{",
-// 				"<{([{{}}[<[[[<>{}]]]>[]]",
-// 			},
-// 			expectedValue: 288957,
-// 		},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run("TestPart2", func(t *testing.T) {
-// 			actual := part2(tc.input)
-
-// 			if actual != tc.expectedValue {
-// 				t.Errorf("Got %d, expected %d", actual, tc.expectedValue)
-// 			}
-
-// 		})
-// 	}
-// }
-
-// func TestFilterOutInvalidLines(t *testing.T) {
-// 	tt := []struct {
-// 		input         []string
-// 		expectedValue []string
-// 	}{
-// 		{
-// 			input: []string{
-// 				"[({(<(())[]>[[{[]{<()<>>",
-// 				"[(()[<>])]({[<{<<[]>>(",
-// 				"{([(<{}[<>[]}>{[]{[(<()>",
-// 				"(((({<>}<{<{<>}{[]{[]{}",
-// 				"[[<[([]))<([[{}[[()]]]",
-// 				"[{[{({}]{}}([{[{{{}}([]",
-// 				"{<[[]]>}<{[{[{[]{()[[[]",
-// 				"[<(<(<(<{}))><([]([]()",
-// 				"<{([([[(<>()){}]>(<<{{",
-// 				"<{([{{}}[<[[[<>{}]]]>[]]",
-// 			},
-// 			expectedValue: []string{
-// 				"[({(<(())[]>[[{[]{<()<>>",
-// 				"[(()[<>])]({[<{<<[]>>(",
-// 				"(((({<>}<{<{<>}{[]{[]{}",
-// 				"{<[[]]>}<{[{[{[]{()[[[]",
-// 				"<{([{{}}[<[[[<>{}]]]>[]]",
-// 			},
-// 		},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run("TestFilterOutInvalidLines", func(t *testing.T) {
-// 			actual := filterOutInvalidLines(tc.input)
-
-// 			for i, v := range actual {
-// 				if v != tc.expectedValue[i] {
-// 					t.Errorf("Got %s, expected %s", actual, tc.expectedValue)
-// 				}
-// 			}
-
-// 		})
-// 	}
-// }
-
-// func TestFindMissingLineEnding(t *testing.T) {
-// 	tt := []struct {
-// 		input         string
-// 		expectedValue string
-// 	}{
-// 		{
-// 			input:         "[",
-// 			expectedValue: "]",
-// 		},
-// 		{
-// 			input:         "[{",
-// 			expectedValue: "}]",
-// 		},
-// 		{
-// 			input:         "[({(<(())[]>[[{[]{<()<>>",
-// 			expectedValue: "}}]])})]",
-// 		},
-// 		{
-// 			input:         "[(()[<>])]({[<{<<[]>>(",
-// 			expectedValue: ")}>]})",
-// 		},
-// 		{
-// 			input:         "(((({<>}<{<{<>}{[]{[]{}",
-// 			expectedValue: "}}>}>))))",
-// 		},
-// 		{
-// 			input:         "{<[[]]>}<{[{[{[]{()[[[]",
-// 			expectedValue: "]]}}]}]}>",
-// 		},
-// 		{
-// 			input:         "<{([{{}}[<[[[<>{}]]]>[]]",
-// 			expectedValue: "])}>",
-// 		},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run("TestFindMissingLineEnding", func(t *testing.T) {
-// 			actual := findMissingLineEnding(tc.input)
-
-// 			if actual != tc.expectedValue {
-// 				t.Errorf("Got %s, expected %s", actual, tc.expectedValue)
-// 			}
-// 		})
-// 	}
-// }
-
-// func TestGetEndingScore(t *testing.T) {
-// 	tt := []struct {
-// 		input         string
-// 		expectedValue int
-// 	}{
-// 		{
-// 			input:         "}}]])})]",
-// 			expectedValue: 288957,
-// 		},
-// 		{
-// 			input:         ")}>]})",
-// 			expectedValue: 5566,
-// 		},
-// 		{
-// 			input:         "}}>}>))))",
-// 			expectedValue: 1480781,
-// 		},
-// 		{
-// 			input:         "]]}}]}]}>",
-// 			expectedValue: 995444,
-// 		},
-// 		{
-// 			input:         "])}>",
-// 			expectedValue: 294,
-// 		},
-// 	}
-
-// 	for _, tc := range tt {
-// 		t.Run("TestGetEndingScore", func(t *testing.T) {
-// 			actual := getEndingScore(tc.input)
-
-// 			if actual != tc.expectedValue {
-// 				t.Errorf("Got %d, expected %d", actual, tc.expectedValue)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tc := range tt {
+		t.Run("TestApplyRules 40 Times", func(t *testing.T) {
+			actual := applyRules(tc.input, tc.rules, 40)
+			if actual != tc.expectedValue {
+				t.Errorf("Got %v, expected %v", actual, tc.expectedValue)
+			}
+		})
+	}
+}
