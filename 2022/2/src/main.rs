079deaed566d9aa40a8b9ecf7338920803a01072
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-12-02 23:12:21 +0100

    2022: day two

---
+++ 2022/2/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let result_one: i32 = contents
+        .split("\n")
+        .map(|positions| match positions {
+            "A X" => 4,
+            "B X" => 1,
+            "C X" => 7,
+            "A Y" => 8,
+            "B Y" => 5,
+            "C Y" => 2,
+            "A Z" => 3,
+            "B Z" => 9,
+            "C Z" => 6,
+            _ => 0,
+        })
+        .sum();
+
+    let result_two: i32 = contents
+        .split("\n")
+        .map(|positions| match positions {
+            "A X" => 3,
+            "B X" => 1,
+            "C X" => 2,
+            "A Y" => 4,
+            "B Y" => 5,
+            "C Y" => 6,
+            "A Z" => 8,
+            "B Z" => 9,
+            "C Z" => 7,
+            _ => 0,
+        })
+        .sum();
+
+    println!("Part One {}", result_one);
+    println!("Part Two {}", result_two);
+}
