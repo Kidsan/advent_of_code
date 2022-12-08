9a4f4d76123ff418c06e8cc4df12be732b4f73c7
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-12-08 21:34:26 +0100

    2022: day 4
    Update solution to add a trait to RangeInclusive which allows checking
    for ranges containing each other or overlapping.
    
    Taken from https://fasterthanli.me/series/advent-of-code-2022/part-4

--- 2022/4/src/main.rs
+++ 2022/4/src/main.rs
@@ -1,3 +1,32 @@
+use std::ops::RangeInclusive;
+
+trait InclusiveRangeExt {
+    fn contains_range(&self, other: &Self) -> bool;
+
+    fn contains_or_is_contained(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
+    }
+
+    fn overlaps(&self, other: &Self) -> bool;
+
+    fn overlaps_or_is_overlapped(&self, other: &Self) -> bool {
+        self.overlaps(other) || other.overlaps(self)
+    }
+}
+
+impl<T> InclusiveRangeExt for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.start()) || self.contains(other.end())
+    }
+}
+
 fn main() {
     let contents = include_str!("../input.txt").lines().map(|line| {
         let parts: Vec<&str> = line.split(",").collect();
@@ -14,66 +43,21 @@ fn main() {
         let right_parsed_end = right_contraints[1].parse::<i32>().unwrap();
 
         return (
-            (parsed_start, parsed_end),
-            (right_parsed_start, right_parsed_end),
+            parsed_start..=parsed_end,
+            right_parsed_start..=right_parsed_end,
         );
     });
 
     let result_one = contents
         .clone()
-        .map(|parsed_line| entire_overlap(parsed_line.0, parsed_line.1))
-        .filter(|has_overlap| *has_overlap)
+        .filter(|(a, b)| a.contains_or_is_contained(&b))
         .count();
 
     let result_two = contents
         .clone()
-        .map(|parsed_line| partial_overlap(parsed_line.0, parsed_line.1))
-        .filter(|has_overlap| *has_overlap)
+        .filter(|(a, b)| a.overlaps_or_is_overlapped(&b))
         .count();
 
     println!("Part One {}", result_one);
     println!("Part Two {}", result_two);
-}
-
-fn entire_overlap(left: (i32, i32), right: (i32, i32)) -> bool {
-    // left contains right
-    if left.0 <= right.0 && left.1 >= right.1 {
-        return true;
-    }
-
-    // right contains left
-    if right.0 <= left.0 && right.1 >= left.1 {
-        return true;
-    }
-    false
-}
-
-fn partial_overlap(left: (i32, i32), right: (i32, i32)) -> bool {
-    for pos in left.0..=left.1 {
-        if pos >= right.0 && pos <= right.1 {
-            return true;
-        }
-    }
-    false
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_entire_overlap() {
-        assert_eq!(entire_overlap((2, 8), (3, 7)), true);
-        assert_eq!(entire_overlap((6, 6), (4, 6)), true);
-        assert_eq!(entire_overlap((2, 3), (4, 6)), false);
-    }
-
-    #[test]
-    fn test_partial_overlap() {
-        assert_eq!(partial_overlap((2, 8), (3, 7)), true);
-        assert_eq!(partial_overlap((6, 6), (4, 6)), true);
-        assert_eq!(partial_overlap((2, 3), (4, 6)), false);
-
-        assert_eq!(partial_overlap((2, 6), (4, 8)), true);
-    }
 }
