7b46bdafe733114d255399f5e09c0ff915850a11
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-11-19 21:39:32 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/4172cdda7e56a48065475fb98c57b03b83c1fde4' (2022-12-16)
      → 'github:NixOS/nixpkgs/53aeb5c67b78a1bb565467043b79c3948afacbc9' (2023-11-19)
    • Updated input 'utils':
        'github:numtide/flake-utils/5aed5285a952e0b949eb3ba02c12fa4fcfef535f' (2022-11-02)
      → 'github:numtide/flake-utils/ff7b65b44d01cf9ba6a71320833626af21126384' (2023-09-12)
    • Added input 'utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e' (2023-04-09)

--- flake.nix
+++ flake.nix
@@ -1,36 +1,30 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    gomod2nix.url = "github:nix-community/gomod2nix";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, gomod2nix}:
+  outputs = { self, nixpkgs, utils, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ gomod2nix.overlays.default]; };
-        naersk-lib = pkgs.callPackage naersk { };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
-
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
-
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy bashInteractive
-            pkgs.gomod2nix
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [ pkgs.openssl ];
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            bashInteractive
             pkg-config
-            glibc
-            buildPackages.go_1_19
-            buildPackages.gopls
-            buildPackages.gotools
-            buildPackages.go-tools
-            buildPackages.delve
-	    nixpkgs-fmt
-	    ];
+            buildPackages.go_1_21
+            nixpkgs-fmt
+          ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
